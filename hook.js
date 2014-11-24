512984988b44d907925a2228fd4a0649749fed47
ac_airbear <acolby@live.com>
2014-11-23 17:13:03 -0800

    added generalized hook

--- hook.js
+++ hook.js
@@ -1,7 +1,7 @@
 // import the setup file
 var setup = {
-	'github':{
-		'repo': 'acolby/platosPlate',
+	'github': {
+		'repo': 'owner/reponame', // locaion of the repo on github
 		'branch': 'master',
 		'remote': 'origin'
 	},
@@ -10,26 +10,65 @@ var setup = {
 };
 
 var gith = require('gith').create(setup.listingPort);
-var shell = require('shelljs');
+var Q = require('q');
+var exec = require('child_process').exec;
 
-shell.echo('Sorry, this script requires git');
+doesSystemHaveGit().then(
+	//yes
+	function(sucObj){
+		gith({
+			repo: setup.github.repo
+		}).on('all', function(payload) {
+			pullNewCommitFromGithub(payload).then(
+				//success
+				function(sucObj){
+					console.log('success updating repot');
+				},
+				//error
+				function(errObj){
+					console.log('oops there was an error');
+				}
+			);
+		});
+		console.log('here');
+	}, 
+	//no
+	function(errObj){
+		console.log('you don\'t have git installed');
+		process.exit(1);
+	}
+);
 
-/*
-if (!shell.which('git')) {
-	shell.echo('Sorry, this script requires git');
-	shell.exit(1);
-} else {
-	gith({
-		repo: setup.github.repo
-	}).on('all', function(payload) {
-		if (payload.branch === setup.github.branch) {
-			shell.cd('..');
-			shell.cd(setup.github.repo);
-
-			shell.git('fetch ' + setup.github.remote + ' ' + setup.github.branch);
-
-			shell.git('checkout')
+function doesSystemHaveGit() {
+	var defer = Q.defer();
+	exec('which git', function(error, stdout, stderr) {
+		if( stdout.length > 0){
+			defer.resolve();
+		}else{
+			defer.reject();
 		}
 	});
+	return defer.promise;
 }
-*/
+
+function pullNewCommitFromGithub(payload){
+	var defer = Q.defer();
+	if (payload.branch === setup.github.branch) {
+		var command = [
+			'cd ' + setup.path,
+			'git fetch ' + setup.github.remote + ' ' + setup.github.branch,
+			'git checkout ' + setup.github.branch,
+			'git reset --hard ' + setup.github.remote + '/' + setup.github.branch
+		].join(' && ');
+		exec(command, function(error, stdout, stderr) {
+			if(stdout.error > 0){
+				defer.reject();
+			}else{
+				defer.resolve();
+			}
+		});
+	}else{
+		defer.reject();
+	}
+	return defer.promise;
+}
